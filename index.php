ccd13b02501916ca30bca86024e634e072a370ef
Mike Haw <mikehaw@gmail.com>
2014-09-15 23:34:40 -0400

    Updated index to use BootStrap, started work on template, major work yet to be done on the navBar functionality

--- index.php
+++ index.php
@@ -1,28 +1,135 @@
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Mike Haw - DWA-15 - Fall 2014</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mike Haw - DWA-15 - Fall 2014</title>
 
-	<body>
-		<h2>Mike Haw - DWA-15 - Fall 2014</h2>	
-		<img src="images\jmh.jpg">
-		<h5>Mike on the Summit of Mt. Adam, August 2012</h5>
-	
-		<h4>In 2+ sentences describe who you are and what you do.</h4>
-		<p>I am a student at the Harvard Extension School working towards an ALM in Information Technology. In my professional life I am project manager for a legal technology services company. In my free time I enjoy 			spending time with my wife and dog, cooking, and occasionally climbing mountains. </p>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-		<h4>Why are you taking this course?</h4>
-		<p>I am taking this course to add to my web development and programming toolbox.</p>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-		<h4>What previous experience do you have with programming?</h4>
-		<p>Experience with Java, data structures, and basic web development.</p>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Home</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <div class="dropdown">
+							<button class="btn btn-inverse dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
+							Project 1
+							<span class="caret"></span>
+							</button>
+								<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+									<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Live Link</a></li>
+									<li role="presentation"><a role="menuitem" tabindex="-1" href="#">GitHub</a></li>
+								</ul>
+							</div>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    
+    <br>
 
-		<h4>Are you working on a Mac, PC, or some other platform?</h4>
-		<p>Primarily working on a Mac, with occasional PC usage.</p>
+    <!-- Page Content -->
+    <div class="container">
 
-	</body>
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Mike Haw
+                    <small>DWA-15, Fall 2014</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-8">
+                <img class="img-responsive" src="images/jmh.jpg" alt="Summit of Mt. Adams, August 2012">
+            </div>
+
+            <div class="col-md-4">
+                <h3>About</h3>
+                <p>I am a student at the Harvard Extension School working towards an ALM in Information Technology. In my professional life I am project manager for a legal technology services company. In my free time I enjoy spending time with my wife and dog, cooking, and occasionally climbing mountains.</p>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Related Projects Row -->
+        <div class="row">
+
+            <div class="col-sm-4 col-xs-6">
+                    <h4>Why are you taking this course?</h4>
+		<div class="well"><p>I am taking this course to add to my web development and programming toolbox.</p></div>
+            </div>
+
+            <div class="col-sm-4 col-xs-6">
+                    		<h4>Previous programming experience?</h4>
+		<div class="well"><p>Experience with Java, data structures, and basic web development.</p></div>
+            </div>
+
+            <div class="col-sm-4 col-xs-6">
+                    <h4>Development environment?</h4>
+		<div class="well"><p>Primarily working on a Mac, with occasional PC usage.</p></div>
+            </div>
 
 
+        </div>
+        <!-- /.row -->
 
+        <hr>
+		
+		<!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="btn-group btn-group-justified">
+					<div class="btn-group">
+						<a href="https://twitter.com/guyfromva"><button type="button" class="btn btn-default">Twitter</button></a>
+					</div>
+					<div class="btn-group">
+						<a href="https://github.com/mhaw"><button type="button" class="btn btn-default">GitHub</button></a>
+					</div>
+					<div class="btn-group">
+						<a href="http://www.linkedin.com/in/mikehaw"><button type="button" class="btn btn-default">LinkedIn</button></a>
+					</div>
+			</div>
+          </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
 </html>
